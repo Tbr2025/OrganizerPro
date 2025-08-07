b41edda87d95d632e918aa33bbc4bdc8d7847bc8
Navas Fazil <navasfazil@gmail.com>
2025-08-07 20:10:17 +0530

    player

--- resources/scripts/remove_bg.py
+++ resources/scripts/remove_bg.py
@@ -1,6 +1,7 @@
 import sys
 from rembg import remove
 from PIL import Image
+import os
 
 if len(sys.argv) != 3:
     print("Usage: remove_bg.py input_path output_path")
@@ -9,7 +10,15 @@ if len(sys.argv) != 3:
 input_path = sys.argv[1]
 output_path = sys.argv[2]
 
-with Image.open(input_path) as input_image:
-    output_image = remove(input_image)
-    output_image.save(output_path)
-    print(f"Background removed and saved to {output_path}")
+if not os.path.exists(input_path):
+    print(f"Input file does not exist: {input_path}")
+    sys.exit(1)
+
+try:
+    with Image.open(input_path) as input_image:
+        output_image = remove(input_image)
+        output_image.save(output_path)
+        print(f"Background removed and saved to {output_path}")
+except Exception as e:
+    print(f"Error: {str(e)}")
+    sys.exit(1)
