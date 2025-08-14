82c38fabce02ee47d66544a9eb11288c6f7670cf
Navas Fazil <navasfazil@gmail.com>
2025-08-15 02:10:12 +0530

    server broacoasig fx

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,24 +1,13 @@
-import axios from 'axios';
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-// This is the part that creates window.Echo
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
+import axios from 'axios';
+
+window.axios = axios;
+axios.defaults.withCredentials = true; // send cookies
+axios.defaults.headers.common['X-CSRF-TOKEN'] =
+  document.querySelector('meta[name="csrf-token"]').content;
 
 window.Pusher = Pusher;
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: import.meta.env.VITE_PUSHER_APP_KEY,
-//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-//     wsHost: window.location.hostname,
-//     wsPort: 6001,
-//     forceTLS: false,
-//     disableStats: true,
-//     enabledTransports: ['ws', 'wss'],
-// });
-
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
@@ -29,18 +18,9 @@ window.Echo = new Echo({
     forceTLS: false,
     disableStats: true,
     enabledTransports: ['ws', 'wss'],
-    authorizer: (channel, options) => {
-        return {
-            authorize: (socketId, callback) => {
-                axios.post('/broadcasting/auth', {
-                    socket_id: socketId,
-                    channel_name: channel.name
-                }).then(response => {
-                    callback(false, response.data);
-                }).catch(error => {
-                    callback(true, error);
-                });
-            }
-        };
+    auth: {
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').content
+        }
     }
 });
