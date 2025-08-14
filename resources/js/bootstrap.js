220c496f58252a673e3ff5502ddb706019f34cac
Navas Fazil <navasfazil@gmail.com>
2025-08-15 03:00:52 +0530

    server broacoasig fx

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,13 +8,22 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
+window.Pusher = Pusher;
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
+    wsHost: import.meta.env.VITE_PUSHER_HOST,
+    wsPort: import.meta.env.VITE_PUSHER_PORT,
     forceTLS: false,
+    encrypted: false,
     disableStats: true,
     enabledTransports: ['ws', 'wss'],
+    auth: {
+        headers: {
+            'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+            'Authorization': 'Bearer ' + window.Laravel?.apiToken || ''
+        }
+    }
 });
