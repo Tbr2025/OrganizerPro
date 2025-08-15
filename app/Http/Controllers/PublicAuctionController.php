2bb7554c2686f8ea2ec568189cd4f3ac1bd88e24
Navas Fazil <navasfazil@gmail.com>
2025-08-15 20:44:06 +0530

    fix

--- app/Http/Controllers/PublicAuctionController.php
+++ app/Http/Controllers/PublicAuctionController.php
@@ -65,4 +65,41 @@ class PublicAuctionController extends Controller
             ] : null,
         ]);
     }
+
+
+
+       public function soldPlayer(Auction $auction)
+    {
+        $player = $auction->auctionPlayers()
+            ->with([
+                'player',
+                'player.playerType',
+                'player.battingProfile',
+                'player.bowlingProfile',
+                'soldToTeam', // This is needed for team logo
+                'bids',
+                'bids.team'
+            ])
+            ->whereIn('status', ['sold']) // include sold players
+            ->orderBy('status', 'desc') // optionally show 'on_auction' first
+            ->first();
+
+        return response()->json([
+            'success' => true,
+            'auctionPlayer' => $player ? [
+                'id' => $player->id,
+                'player' => $player->player,
+                'current_price' => $player->current_price,
+                'current_bid_team' => $player->current_bid_team,
+                'bids' => $player->bids,
+                'status' => $player->status,
+                'sold_to_team' => $player->soldToTeam ? [
+                    'name' => $player->soldToTeam->name,
+                    'logo_path' => $player->soldToTeam->team_logo
+                        ? asset('storage/' . $player->soldToTeam->team_logo)
+                        : null,
+                ] : null,
+            ] : null,
+        ]);
+    }
 }
