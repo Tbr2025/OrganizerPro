a82b4f057bdf0d106144f566f62243663a9c7f2a
Navas Fazil <navasfazil@gmail.com>
2025-08-15 16:57:54 +0530

    fix budget - tema manger fix

--- app/Services/RolesService.php
+++ app/Services/RolesService.php
@@ -313,10 +313,11 @@ class RolesService
             'dashboard.view',
             'actual-team.view', // Can view teams (at least their own)
             'actual-team.edit', // Can edit their own team details (if allowed by policy)
-
+            'auction.closed-bids',
             'player.view', // Can view players (at least their team's)
             'match.view',
-           
+            'auction.view',
+
         ];
         $roles['team_manager'] = $this->createOrSyncRole('Team Manager', $teamManagerPermissions);
 
